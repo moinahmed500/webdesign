46f134dd373dd3cfe6ebec68ebcd9eb339fb8e56
SHARJEE-QURESHI-2021 <sharjeelqureshi2021@gmail.com>
2024-03-04 22:17:13 +0500

    hahaha

---
+++ index.js
@@ -0,0 +1,97 @@
+//Alert comment
+// alert ("hello moin i am a developer")
+
+// let firstName = "moin-ahmed";
+// console.log (firstName);
+// alert ("hello moin /n i am a developer");
+
+// alert (5/5);
+// alert (5-4);
+// alert (5*5);
+// alert ("Here is you some"+ (5*5));
+// alert ("Here is you some"+ (5+5));
+// alert ("Here is you some"+ (5-5));
+// alert ("Here is you some"+ (5/5));
+//var num1 = 40;
+//var num2 = 50;
+//var addmassage = "here is you percentage";
+
+//alert (addmassage + num1 / num2 * 100 +("%"))
+// var percentage = 45/50*100;
+// alert (percentage)
+//var mealschdule = "Break fast : = Egg With Bread"
+//alert (mealschdule)
+//mealschdule = "lunch:= biryani With Raita"
+//alert(mealschdule)
+//mealschdule = "Dinner:= Tikka & kebabs"
+//alert(mealschdule)
+//var myDadName = "My DAD Name Is Mohammad Shamim"
+//alert (myDadName)
+//var sum = 2+2
+//alert (sum)
+//sum = 2-2
+//alert (sum)
+//sum = 2*5
+//alert (sum)
+//try {
+ //   sum
+//} catch (error) {
+
+//} 5/2
+//alert(sum)
+
+// age calculator
+
+//var myAge = 2024-1984
+//alert (myAge)
+//var secondof24hour = 60*60*24
+//alert(secondof24hour)
+
+//modulus opretor
+//alert (6%9)
+
+//increment oprator
+//var currentYear = 2024
+//alert(currentYear)
+//currentYear++
+
+//alert (currentYear+1)
+//decrement oprator
+//currentYear--
+//var updatedYear = ++currentYear
+//alert(currentYear)
+
+//alert (updatedYear)
+ //var matH = 3+2*4
+ //alert (matH)
+ //result 11
+
+ //var mooth = (2+3)*7
+ //alert (mooth)
+ //result 35
+//var myana = 4*(5+7)
+//alert (myana)
+//result 48
+//var mina = 6/2+3
+//alert (mina)
+//result 6
+
+// age results
+//var useAge = -prompt ("Enter your Age")
+//alert (useAge-)
+
+//concatination
+//var firstName =prompt ("Enter your First Name")
+//var lastName =prompt ("Enter Your last Name")
+//var fullName = firstName" + +"lastName;
+//console.log(fullName)
+
+//function
+function sum(){
+    var num1 = +prompt ("Enter First Number")
+    var num2 = +prompt ("Enter Second Number")
+    console.log(num1 + num2)
+}
+sum()
+sum()
+sum()
